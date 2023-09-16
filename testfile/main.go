e86dc4a39642090bf96b8db9df44ff7aeb09a8f2
thebarsum <artem2008z@yandex.ru>
2023-09-16 15:54:02 +0300

    1

--- testfile/main.go
+++ testfile/main.go
@@ -1,8 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"test/mymath"
+)
 
+func Sqrt(x float64) float64 {
+	return math.Sqrt(x)
+}
+func Ceil(x float64) float64 {
+	return math.Ceil(x)
+}
+func Floor(x float64) float64 {
+
+	return math.Floor(x)
+}
+func Pow(x, y float64) float64 {
+	return math.Pow(x, y)
+}
+func Max(x, y float64) float64 {
+	return math.Max(x, y)
+}
+func Min(x, y float64) float64 {
+	return math.Min(x, y)
+}
 func main() {
-	fmt.Println("HelloWorld!")
+	fmt.Println(mymath.Abs(1))
 
 }
