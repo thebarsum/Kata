0dfc41b002dee8ba06c71094a61468756b60fbe0
thebarsum <artem2008z@yandex.ru>
2023-09-16 15:25:21 +0300

    1

--- mymath/mymath.go
+++ mymath/mymath.go
@@ -1,6 +1,8 @@
 package mymath
 
-import "math"
+import (
+	"math"
+)
 
 func Sqrt(x float64) float64 {
 	return math.Sqrt(x)
@@ -20,4 +22,16 @@ func Max(x, y float64) float64 {
 }
 func Min(x, y float64) float64 {
 	return math.Min(x, y)
+}
+func Abs(x float64) float64 {
+	return math.Abs(x)
+}
+func Acos(x float64) float64 {
+	return math.Acos(x)
+}
+func Acosh(x float64) float64 {
+	return math.Acosh(x)
+}
+func Asin(x float64) float64 {
+	return math.Asin(x)
 }
